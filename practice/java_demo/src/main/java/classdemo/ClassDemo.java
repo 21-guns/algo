75cae30721b3d0bf06fcf8c135232dbd75bb8535
loopsun <zhouhua2297@yeah.net>
2018-05-16 23:37:00 +0800

    Feature: java init commit

--- practice/java_demo/src/main/java/classdemo/ClassDemo.java
+++ practice/java_demo/src/main/java/classdemo/ClassDemo.java
@@ -1,5 +1,8 @@
 package classdemo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassDemo {
     final int months = 12;
     public String name;
@@ -22,8 +25,25 @@ public class ClassDemo {
 
     public static void main(String[] args) {
         Employee employee = new Employee("Max", "Dev");
+        Employee employee2 = new Employee("Max", "Dev");
+        boolean equal1 = employee == employee2;
         System.out.println(employee.name);
         System.out.println(employee.position);
+        System.out.println(equal1);
+
+        Statistic statistic = new Statistic();
+        Game heros = new Game("Heros");
+        Game doom = new Game("Doom");
+
+        heros.addListener(statistic);
+        doom.addListener(statistic);
+
+        heros.start();
+        doom.start();
+
+        System.out.println(statistic.lastName);
+        System.out.println(statistic.startsCount);
+
 
     }
 
@@ -67,4 +87,32 @@ class Employee extends Man {
     }
 }
 
+interface GameListener {
+    void gameStarted(String name);
+}
 
+class Game {
+    private List<GameListener> listeners = new ArrayList<GameListener>();
+    public void addListener(GameListener listener) {
+        listeners.add(listener);
+    }
+
+    public String name;
+    public void start() {
+        for (GameListener listener: listeners) listener.gameStarted(name);
+    }
+
+    public Game(String name) {
+        this.name = name;
+    }
+}
+
+class Statistic implements GameListener {
+    public int startsCount;
+    public String lastName;
+
+    public void gameStarted(String name) {
+        startsCount ++;
+        lastName = name;
+    }
+}
